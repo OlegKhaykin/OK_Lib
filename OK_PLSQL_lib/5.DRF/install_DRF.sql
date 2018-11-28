4d7d74802b1f2230dcb26b65df3725625241cbaf
Oleg Khaykin <OlegKhaykin@gmail.com>
2018-11-28 12:46:38 -0500

    OK

---
+++ OK_PLSQL_lib/5.DRF/install_DRF.sql
@@ -0,0 +1,8 @@
+prompt 1.DRF_tables.sql
+@@1.DRF_tables.sql
+prompt 2.SCHEDULER_EXEC_TASK.sql
+@@2.SCHEDULER_EXEC_TASK.sql
+prompt 3.PKG_DATA_REFRESH.pks
+@@3.PKG_DATA_REFRESH.pks
+prompt 4.PKG_DATA_REFRESH.pkb
+@@4.PKG_DATA_REFRESH.pkb
