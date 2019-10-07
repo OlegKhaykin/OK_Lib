8d52a22fdd5ef48bb88d55a76ea3deb59b07df82
OlegKhaykin <OlegKhaykin@gmail.com>
2019-10-06 21:38:43 -0400

    Added MIT license

--- OK_PLSQL_lib/4.ETL_UTILS/02.PKG_ETL_UTILS_pks.sql
+++ OK_PLSQL_lib/4.ETL_UTILS/02.PKG_ETL_UTILS_pks.sql
@@ -1,13 +1,30 @@
 CREATE OR REPLACE PACKAGE pkg_etl_utils AS
 /*
-  =================================================================================================
+Copyright (c) 2019 Oleg Khaykin.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.  
+
+=================================================================================================
   Package PKG_ETL_UTILS contains procedures for performing ETL operations.
   There are two main procedures in this package: 1)ADD_DATA and 2)DELETE_DATA.
   Please, see their descriptions below.
 
-  The package was developed by Oleg Khaykin: 1-201-625-3161. OlegKhaykin@gmail.com. 
-  Your are free to use and change it as you wish.
- 
   History of changes (newest to oldest):
   -------------------------------------------------------------------------------------------------
   10-Feb-2019, OK: added procedure CLEAR_PARAMETER;
--- OraAdmin/Storage/Space.sql
+++ OraAdmin/Storage/Space.sql
@@ -4,10 +4,10 @@ alter session enable parallel ddl;
 --CREATE TABLE tst_ok_space_usage PARALLEL 16 AS 
 SELECT
   d.tablespace_name, d.max_mb, d.allocated_mb,
-  NVL(s.used_mb, 0) used_mb, 
+--  NVL(s.used_mb, 0) used_mb, 
   NVL(f.available_mb, 0) available_mb,
-  ROUND(NVL(f.available_mb, 0)/d.allocated_mb*100) allocated_free_pct, 
-  ROUND((d.max_mb - NVL(s.used_mb, 0))*100/d.max_mb) max_free_pct
+  ROUND(NVL(f.available_mb, 0)/d.allocated_mb*100) allocated_free_pct--, 
+--  ROUND((d.max_mb - NVL(s.used_mb, 0))*100/d.max_mb) max_free_pct
 FROM
   (
     SELECT
@@ -17,7 +17,7 @@ FROM
     FROM dba_data_files
     WHERE 1=1
 --    and tablespace_name NOT IN (SELECT tablespace_name FROM dba_data_files WHERE autoextensible = 'YES')
---    and tablespace_name like 'UNDO%'
+    and tablespace_name = 'INCV_DATA'
     GROUP BY tablespace_name
   ) d,
   (
@@ -34,7 +34,8 @@ FROM
     FROM dba_free_space
     GROUP BY tablespace_name
   ) f
-WHERE s.tablespace_name(+) = d.tablespace_name
+WHERE 1=1
+--and s.tablespace_name(+) = d.tablespace_name
 AND f.tablespace_name(+) = d.tablespace_name
 --and nvl(f.available_mb,0)/d.total_mb < 0.05
 ;
