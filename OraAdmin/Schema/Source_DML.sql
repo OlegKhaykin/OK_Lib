c38afa05d2980dcb8aa94c1cb586020481c242eb
OlegKhaykin <OlegKhaykin@gmail.com>
2019-12-05 10:10:33 -0500

    OK

--- OraAdmin/Schema/Source_DML.sql
+++ OraAdmin/Schema/Source_DML.sql
@@ -1,48 +1,74 @@
-select q.table_name, q.type, q.name, q.dml_operation, q.dml_line, q.table_line
-from
-(
-  select
-    sq.table_name, sq.type, sq.name, sq.table_line,
-    nvl(sq.dml_operation, lag(sq.dml_operation) over(partition by sq.type, sq.name order by sq.line)) dml_operation,  
-    nvl(sq.dml_line, lag(sq.dml_line) over(partition by sq.type, sq.name order by sq.line)) dml_line  
+with
+  dml_info as
+  (
+  select q.table_name, q.type, q.name, q.dml_operation, q.dml_line, q.table_line
   from
   (
     select
-      s.type, s.name, s.line,
-      case 
-        when upper(s.text) like '%INSERT%' then 'INSERT'
-        when upper(s.text) like '%UPDATE%' then 'UPDATE'
-        when upper(s.text) like '%DELETE%' then 'DELETE'
-        when upper(s.text) like '%MERGE%'  then 'MERGE'
-        when upper(s.text) like '%TRUNCATE%' then 'TRUNCATE'
-      end dml_operation,
-      case
-       when 
+      sq.table_name, sq.type, sq.name, sq.table_line,
+      nvl(sq.dml_operation, lag(sq.dml_operation) over(partition by sq.type, sq.name order by sq.line)) dml_operation,  
+      nvl(sq.dml_line, lag(sq.dml_line) over(partition by sq.type, sq.name order by sq.line)) dml_line  
+    from
+    (
+      select
+        s.type, s.name, s.line,
+        case 
+          when upper(s.text) like '%INSERT%' then 'INSERT'
+          when upper(s.text) like '%UPDATE%' then 'UPDATE'
+          when upper(s.text) like '%DELETE%' then 'DELETE'
+          when upper(s.text) like '%MERGE%'  then 'MERGE'
+          when upper(s.text) like '%TRUNCATE%' then 'TRUNCATE'
+        end dml_operation,
+        case
+         when 
+          upper(s.text) like '%INSERT%'   or 
+          upper(s.text) like '%UPDATE%'   or 
+          upper(s.text) like '%DELETE%'   or 
+          upper(s.text) like '%MERGE%'    or 
+          upper(s.text) like '%TRUNCATE%' 
+         then s.line 
+        end dml_line, 
+        t.table_name,
+        nvl2(t.table_name, s.line, null) table_line
+      from dba_source s
+      left join dba_tables t
+        on t.owner = s.owner
+       and regexp_like(upper(s.text), '(\.|\s|^)'||t.table_name||'(\s|$)') 
+       and t.table_name in 
+       (
+        'ACTIVITY',
+        'ACTIVITYDETAIL',
+        'ACTIVITYGROUP',
+        'ACTIVITYGROUPACTIVITYXREF',
+        'DBUG_SETINCENTIVECONFIGURATION',
+        'INCENTIVEATTRIBUTE',
+        'INCENTIVEELIGIBILITY',
+        'INCENTIVEGOALTYPE',
+        'INCENTIVEGOALVALUE',
+        'INCENTIVEMESSAGE',
+        'INCENTIVEMESSAGEATTR',
+        'INCENTIVEOVERRIDE',
+        'INCVSQLSTATEMENT_TMP',
+        'PROGRAM',
+        'RELATEDACTIVITYXREF',
+        'REWARD',
+        'REWARDACTIVITYGROUPXREF',
+        'SUPPLIERPROGRAMASSOC'
+      )
+      where s.owner = 'INCV'
+      and
+      (
         upper(s.text) like '%INSERT%'   or 
         upper(s.text) like '%UPDATE%'   or 
         upper(s.text) like '%DELETE%'   or 
         upper(s.text) like '%MERGE%'    or 
-        upper(s.text) like '%TRUNCATE%' 
-       then s.line 
-      end dml_line, 
-      t.table_name,
-      nvl2(t.table_name, s.line, null) table_line
-    from dba_source s
-    left join dba_tables t
-      on t.owner = s.owner
-     and upper(s.text) like '%'||t.table_name||'%' 
-     and t.table_name in ('MEMBERACTIVITYOCCURRENCE','MEMBERACTIVITYOCCURVAL')
-    where s.owner = 'INCV'
-    and
-    (
-      upper(s.text) like '%INSERT%'   or 
-      upper(s.text) like '%UPDATE%'   or 
-      upper(s.text) like '%DELETE%'   or 
-      upper(s.text) like '%MERGE%'    or 
-      upper(s.text) like '%TRUNCATE%' or 
-      t.table_name is not null
-    )
-  ) sq
-) q
-where q.table_name is not null and q.dml_line > (q.table_line-3)
-order by q.table_name, q.type, q.name, q.table_line; 
+        upper(s.text) like '%TRUNCATE%' or 
+        t.table_name is not null
+      )
+    ) sq
+  ) q
+  where q.table_name is not null and q.dml_line > (q.table_line-3)
+  and q.name = 'INCV_PROGRAMCONFIGURATION_PKG'
+)
+--select * from dml_info order by table_name, type, name, table_line;
+select table_name, listagg(dml_operation, ',') within group(order by dml_operation) from (select distinct table_name, dml_operation from dml_info) group by table_name order by table_name;
--- OraAdmin/Schema/depend.sql
+++ OraAdmin/Schema/depend.sql
@@ -1,13 +1,23 @@
 SET VERIFY OFF
-ACCEPT obj PROMPT 'Specify the object ("schema.name"): '
 
-TRUNCATE TABLE test;
-INSERT INTO test SELECT * FROM dba_dependencies
+define obj=INCV.INCV_PROGRAMCONFIGURATION_PKG
+
+create table N384433.tst_ok_dependencies as
+--INSERT INTO test 
+SELECT * FROM dba_dependencies
 WHERE owner NOT IN ('SYS','SYSTEM');
 
-SELECT Lpad(' ', Level)||owner||'.'||name
-FROM test
-CONNECT BY PRIOR referenced_name=name
-AND referenced_owner = owner
-START WITH owner = Upper(Substr('&obj', 1, Instr('&obj','.')-1))
-AND name = Upper(Substr('&obj', Instr('&obj','.')+1));
+SELECT referenced_name, Lpad(' ', Lvl-1)||type||' '||owner||'.'||name||' depends on '||referenced_type||' '||referenced_owner||'.'||referenced_name explanation
+from
+(
+  select rownum rnum, level lvl, type, owner, name, referenced_type, referenced_owner, referenced_name
+  FROM n384433.tst_ok_dependencies
+  CONNECT BY name = PRIOR referenced_name
+  AND owner = PRIOR referenced_owner
+  AND type = PRIOR referenced_type 
+  START WITH owner = Upper(Substr('&obj', 1, Instr('&obj','.')-1))
+  AND name = Upper(Substr('&obj', Instr('&obj','.')+1))
+)
+--where name = 'INCV_PROGRAMCONFIGURATION_PKG' and referenced_type = 'TABLE'
+--where referenced_type = 'TYPE'
+order by rnum;
