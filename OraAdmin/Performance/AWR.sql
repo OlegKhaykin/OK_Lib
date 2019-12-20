c69502c53e30c1d0dc2bd966a5ef9fd35442e386
OlegKhaykin <OlegKhaykin@gmail.com>
2019-12-20 13:15:09 -0500

    OK

--- OraAdmin/Performance/AWR.sql
+++ OraAdmin/Performance/AWR.sql
@@ -1,19 +1,26 @@
 SELECT output FROM TABLE(DBMS_WORKLOAD_REPOSITORY.AWR_REPORT_HTML(1877720674,  6, 179580, 179783)); 
 
 select
-  s.*
+  distinct
+--  s.begin_interval_time,
+  ss.plan_hash_value
 from dba_hist_snapshot s
 join dba_hist_sqlstat ss on ss.snap_id = s.snap_id and ss.instance_number = s.instance_number
-  and sql_id = '0wd2dktqjsmhh'
---where s.begin_interval_time >= timestamp '2019-12-13 16:00:00' and s.end_interval_time < timestamp '2019-12-13 23:00:00'
-order by s.begin_interval_time desc;
+  and ss.sql_id = '0wd2dktqjsmhh'
+--where s.begin_interval_time >= timestamp '2019-12-06 00:00:00' and s.end_interval_time < timestamp '2019-12-06 23:59:59'
+--order by s.begin_interval_time
+;
 
 select --+ parallel(32)
   * 
 from dba_hist_sqltext
+where sql_id = '0wd2dktqjsmhh';
 where sql_text like '%SELECT /*+ ordered */%CAREENGINEMEMBERPROCESSSTATUS%'
 ;
 
+
+select * from 
+where  = '1693424455'
 
 select * from dba_hist_sql_bind_metadata;
 
--- OraAdmin/Schema/depend.sql
+++ OraAdmin/Schema/depend.sql
@@ -1,22 +1,27 @@
 SET VERIFY OFF
 
-define obj=INCV.INCV_PROGRAMCONFIGURATION_PKG
+define obj=CSID.CARPKG_MHSINFO_120;
 
 create table N384433.tst_ok_dependencies as
 SELECT * FROM dba_dependencies
 WHERE owner NOT IN ('SYS','SYSTEM');
 
-SELECT referenced_name, Lpad(' ', Lvl-1)||type||' '||owner||'.'||name||' depends on '||referenced_type||' '||referenced_owner||'.'||referenced_name explanation
+SELECT
+  q.owner, q.referenced_name,
+  t.num_rows, t.last_analyzed
+--  , Lpad(' ', Lvl-1)||type||' '||owner||'.'||name||' depends on '||referenced_type||' '||referenced_owner||'.'||referenced_name explanation
 from
 (
   select rownum rnum, level lvl, type, owner, name, referenced_type, referenced_owner, referenced_name
-  FROM n384433.tst_ok_dependencies
+--  FROM n384433.tst_ok_dependencies
+  from dba_dependencies
   CONNECT BY name = PRIOR referenced_name
   AND owner = PRIOR referenced_owner
   AND type = PRIOR referenced_type 
   START WITH owner = Upper(Substr('&obj', 1, Instr('&obj','.')-1))
   AND name = Upper(Substr('&obj', Instr('&obj','.')+1))
-)
---where name = 'INCV_PROGRAMCONFIGURATION_PKG' and referenced_type = 'TABLE'
-where referenced_type = 'TABLE'
+) q
+join dba_tables t
+  on t.owner = q.owner and t.table_name = q.referenced_name
+where q.referenced_type = 'TABLE'
 order by rnum;
