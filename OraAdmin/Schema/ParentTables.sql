e4709cbd8d90263aa9dd433d0531dfa41cc444d7
OlegKhaykin <OlegKhaykin@gmail.com>
2019-12-29 18:21:57 -0500

    OK

--- OraAdmin/Schema/ParentTables.sql
+++ OraAdmin/Schema/ParentTables.sql
@@ -1,26 +1,51 @@
+-- Multi-level research
+with
+  ref as
+  (
+    select distinct
+      c.owner, c.table_name, r.owner r_owner, r.table_name r_table_name
+    from dba_constraints          c
+    left join dba_constraints     r
+      on r.owner = c.r_owner and r.constraint_name = c.r_constraint_name
+    where c.owner = 'ODS' 
+  )
+select
+  lpad(' ', (level-1)*2) ||
+  owner||'.'||table_name|| ' -> '||r_owner||'.'||r_table_name reference
+from ref r
+connect by owner = prior r_owner and table_name = prior r_table_name 
+start with owner = 'ODS'
+and table_name IN 
+(
+  'PLANSPONSOR','STG_SUPPLIERCONTROL','STG_SUPPLIERCONTROLBPLVXREF','SUPPLIERBATCH',
+  'SUPPLIERERRORDETAIL','SUPPLIERERRORSUMMARY','SUPPLIERORGANIZATION','SUPPLIERPRODUCTAUTOCONFIG'
+);
+  
+-- Single level research:  
 select
   c.owner||'.'||c.table_name||'.'||c.constraint_name||'('||
-  concat_dataset
+  to_char(substr(concat_v2_set(cursor
   (
-    cursor
-    (
-      select column_name from dba_cons_columns
-      where owner = c.owner and constraint_name = c.constraint_name
-      order by position
-    )
-  )||') -> '||
+    select column_name from dba_cons_columns
+    where owner = c.owner and constraint_name = c.constraint_name
+    order by position
+  )), 1, 500))||') -> '||
   r.owner||'.'||r.table_name||'('||
-  concat_dataset
+  to_char(substr(concat_v2_set(cursor
   (
-    cursor
-    (
-      select column_name from dba_cons_columns
-      where owner = r.owner and constraint_name = r.constraint_name
-      order by position
-    )
-  )||')'
-from
-  dba_constraints c,
-  dba_constraints r
-where c.owner = 'SAGE' and c.table_name = Upper('&1') and c.constraint_type = 'R' 
-and c.r_owner = r.owner and c.r_constraint_name = r.constraint_name
+    select column_name from dba_cons_columns
+    where owner = r.owner and constraint_name = r.constraint_name
+    order by position
+  )), 1, 500))||')' reference
+from dba_constraints    c
+join dba_constraints    r
+  on r.owner = c.r_owner
+ and r.constraint_name = c.r_constraint_name
+where c.owner = 'ODS'
+and c.constraint_type = 'R' 
+and c.table_name IN 
+(
+  'PLANSPONSOR','STG_SUPPLIERCONTROL','STG_SUPPLIERCONTROLBPLVXREF','SUPPLIERBATCH',
+  'SUPPLIERERRORDETAIL','SUPPLIERERRORSUMMARY','SUPPLIERORGANIZATION','SUPPLIERPRODUCTAUTOCONFIG'
+)
+order by 1;
