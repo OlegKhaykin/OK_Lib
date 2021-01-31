1a9f80c69d1d51ec43c0e8cb5954a8b0539d63ae
OlegKhaykin <OlegKhaykin@gmail.com>
2021-01-31 11:51:31 -0500

    OK

--- OK_PLSQL_lib/1.Generic/02.SPLIT_STRING.sql
+++ OK_PLSQL_lib/1.Generic/02.SPLIT_STRING.sql
@@ -2,42 +2,35 @@ CREATE OR REPLACE FUNCTION split_string(p_string IN VARCHAR2, p_separator IN VAR
 /*
   History of changes:
   ------------------------------------------------------------------------------
-  06-Sep-2019, OK: re-wrote completely;
-  28-Aug-2019, OK: added logic for processing NULL p_string value
+  29-Jan-2021, OK: new version.
 */
-  ret         tab_v256;
+  n_len       PLS_INTEGER;
   n_sep_len   PLS_INTEGER;
+  p1          PLS_INTEGER;
+  p2          PLS_INTEGER;
+  i           PLS_INTEGER := 0;
+  
+  ret         tab_v256;
 BEGIN
-  IF p_string IS NULL THEN
-    RETURN tab_v256();
-  ELSE
+  ret := tab_v256();
+  
+  IF p_string IS NOT NULL THEN
+    n_len := LENGTH(p_string);
     n_sep_len := LENGTH(p_separator);
+    p1 := 1;
     
-    WITH
-      qry(part, rest, sep_pos) AS
-      (
-        SELECT
-          DECODE(sep_pos, 0, str, SUBSTR(str, 1, sep_pos-1)) part,
-          DECODE(sep_pos, 0, NULL, SUBSTR(str, sep_pos + n_sep_len)) rest,
-          DECODE(sep_pos, 0, 0, INSTR(SUBSTR(str, sep_pos + n_sep_len), p_separator)) sep_pos
-        FROM
-        (
-          SELECT
-            p_string AS str,
-            INSTR(p_string, p_separator) AS sep_pos
-          FROM dual
-        )
-      UNION ALL
-        SELECT
-          DECODE(sep_pos, 0, rest, SUBSTR(rest, 1, sep_pos-1)) part,
-          DECODE(sep_pos, 0, NULL, SUBSTR(rest, sep_pos + n_sep_len)) rest,
-          DECODE(sep_pos, 0, 0, INSTR(SUBSTR(rest, sep_pos + n_sep_len), p_separator)) sep_pos
-        FROM qry 
-        WHERE rest IS NOT NULL
-      )
-    SELECT part BULK COLLECT INTO ret FROM qry;
+    LOOP
+      ret.EXTEND;
+      i := i+1;
+      p2 := INSTR(p_string, p_separator, p1);
+      ret(i) := SUBSTR(p_string, p1, CASE p2 WHEN 0 THEN n_len ELSE p2-p1 END);
+      EXIT WHEN p2 = 0;
+      p1 := p2 + n_sep_len;
+    END LOOP;
   END IF;
   
   RETURN ret;
 END;
 /
+
+GRANT EXECUTE ON split_string TO PUBLIC;
